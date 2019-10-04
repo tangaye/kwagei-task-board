612744219bd0a05be415f6f20348657664edb3a5
Carlos Nah <cnah27@gmail.com>
2019-10-04 11:57:50 +0000

    Created create-task.js

---
+++ js/create-task.js
@@ -0,0 +1,26 @@
+var tasks = [];
+var input_val, task_obj;
+
+/**
+ * Get value from input element when the submit button is clicked
+ */
+document.getElementById('submit').addEventListener('click', function(){
+    input_val = document.getElementById('new_task').value;
+    createTask(input_val);
+})
+
+
+/*
+* @param {string} task - takes in a task 
+* and stores it in the tasks array
+*/
+function createTask(task) {
+    const date = new Date();
+    task_obj = {
+        task: task,
+        date_created: date.now
+    };
+    tasks.push(task_obj);
+    localStorage.setItem('task', JSON.stringify(tasks));
+    return tasks;
+}
