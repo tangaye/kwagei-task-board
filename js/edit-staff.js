1df9cb6acae915e25cfdc309571ca198d17b2f80
boaslah <blessing@kwagei.com>
2019-10-17 14:26:06 +0000

    edited edit-staff.js file for the second time

--- js/edit-staff.js
+++ js/edit-staff.js
@@ -1,10 +1,16 @@
 var buttons = document.querySelectorAll('buttons');
-var full_name_input = document.getElementById('');
-var email_input = document.getElementById('');
-var address_input = document.getElementById('');
-var contact_input = document.getElementById('');
-var DOB_input = document.getElementById('');
+var full_name_input = document.getElementById('edit-staff-fullname');
+var email_input = document.getElementById('edit-staff-email');
+var address_input = document.getElementById('edit-staff-address');
+var contact_input = document.getElementById('edit-staff-phone-number');
+var DOB_input = document.getElementById('edit-staff-dob');
+var staff_id = document.getElementById('edit-staff-id')
 
 buttons.addEventListener('click', function(event) {
     console.log(event.target.id);
+    full_name_input = '';
+    email_input = '';
+    address_input = '';
+    contact_input = '';
+    DOB_input = '';
 })
