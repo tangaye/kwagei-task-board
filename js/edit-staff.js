ef3f59c55ce1454892dab098f7ddb113cb466041
boaslah <blessing@kwagei.com>
2019-10-17 14:04:31 +0000

    edited edit-staff.js file

---
+++ js/edit-staff.js
@@ -0,0 +1,10 @@
+var buttons = document.querySelectorAll('buttons');
+var full_name_input = document.getElementById('');
+var email_input = document.getElementById('');
+var address_input = document.getElementById('');
+var contact_input = document.getElementById('');
+var DOB_input = document.getElementById('');
+
+buttons.addEventListener('click', function(event) {
+    console.log(event);
+})
