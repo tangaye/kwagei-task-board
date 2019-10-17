39f7160cc56b3ff3abb4aa579a1aa600973a784c
boaslah <blessing@kwagei.com>
2019-10-17 15:41:36 +0000

    remodify edit-staff.js file

--- js/edit-staff.js
+++ js/edit-staff.js
@@ -4,18 +4,41 @@ var email_input = document.getElementById('edit-staff-email');
 var address_input = document.getElementById('edit-staff-address');
 var contact_input = document.getElementById('edit-staff-phone-number');
 var DOB_input = document.getElementById('edit-staff-dob');
-// var staff_id = document.getElementById('edit-staff-id')
+var staff_id = document.getElementById('edit-staff-id');
+var staff_data = JSON.parse(localStorage.getItem('staff_data'));
+var button_id = 0;
 
 buttons.forEach(button =>{
     button.addEventListener('click', function(event){
-        console.log(event.target.id);
-        var staff_id = event.target.id;
-        var staff_data = localStorage.getItem('staff_data');
-        console.log(staff_data);
-        full_name_input = staff_data[staff_id].full_name ;
-        email_input = staff_data[staff_id].email;
-        address_input = staff_data[staff_id].address;
-        contact_input = staff_data[staff_id].contact;
-        DOB_input = staff_data[staff_id].dob;
+        button_id = event.target.id;
     })
+})
+
+function editStaff(id){
+    console.log(id)
+    staff_id.value = button_id+1;
+    full_name_input.value = staff_data[id].full_name ;
+    email_input.value = staff_data[id].email;
+    address_input.value = staff_data[id].address;
+    contact_input.value = staff_data[id].phone;
+    DOB_input.value = staff_data[id].dob;
+}
+editStaff(button_id);
+
+function updateStaff(){
+    console.log("Home :", button_id)
+    staff_data[button_id].full_name = full_name_input.value;
+    staff_data[button_id].email = email_input.value;
+    staff_data[button_id].address = address_input.value;
+    staff_data[button_id].phone = contact_input.value;
+    staff_data[button_id].dob = DOB_input.value;
+
+   
+
+    localStorage.setItem('staff_data', JSON.stringify(staff_data));
+}
+
+document.getElementById('update-staff').addEventListener('click', function(event){
+    event.preventDefault()
+    updateStaff()
 })
